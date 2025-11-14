2e7f56906d0a51570231c0f196a8b3e4cfd080f5
Kurma ritish <136564903+Ritish017@users.noreply.github.com>
2025-11-14 16:40:44 +0530

    Add src/nodes/grader.py

---
+++ src/nodes/grader.py
@@ -0,0 +1,24 @@
+﻿from typing import Literal
+from src.states.graphstate import GraphState
+from src.models.grader import grader_model, GradeDocuments, GRADE_PROMPT
+
+def grade_documents(
+    state: GraphState,
+) -> Literal["generate_answer", "rewrite_question"]:
+    """Determine whether the retrieved documents are relevant to the question."""
+    question = state["messages"][0].content
+    context = state["messages"][-1].content
+
+    prompt = GRADE_PROMPT.format(question=question, context=context)
+    response = (
+        grader_model
+        .with_structured_output(GradeDocuments).invoke(  
+            [{"role": "user", "content": prompt}]
+        )
+    )
+    score = response.binary_score
+
+    if score == "yes":
+        return "generate_answer"
+    else:
+        return "rewrite_question"
